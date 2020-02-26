348a77d348830d173230811b85d1e1be86ec9280
Scott Sperling <scsper@gmail.com>
2020-02-26 10:34:51 -0800

    Add fragment matcher mock.

--- src/mockClient.ts
+++ src/mockClient.ts
@@ -11,12 +11,16 @@ export type RequestHandlerResponse<T> = { data: T, errors?: any[] };
 export type MockApolloClient = ApolloClient<NormalizedCacheObject> &
   { setRequestHandler: (query: DocumentNode, handler: RequestHandler) => void };
 
+// Taken from the suggestion here: https://github.com/apollographql/react-apollo/issues/1747#issuecomment-562658518
+const fragmentMatcher = { match: () => true }
+
 export const createMockClient = (): MockApolloClient => {
   const mockLink = new MockLink();
 
   const client = new ApolloClient({
     cache: new Cache({
       addTypename: false, // TODO: Handle addTypename?
+      fragmentMatcher,
     }),
     link: mockLink,
   });
