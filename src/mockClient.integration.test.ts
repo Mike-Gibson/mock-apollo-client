3df63188db4f7ca9204832db88742086174ed2bf
Mike Gibson <mike_gibson@live.co.uk>
2020-02-29 21:40:33 +0000

    Add test for cache readQuery

--- src/mockClient.integration.test.ts
+++ src/mockClient.integration.test.ts
@@ -9,54 +9,87 @@ import { ApolloQueryResult } from 'apollo-client';
 describe('MockClient integration tests', () => {
   let mockClient: MockApolloClient;
 
-  const queryOne = gql`query One {one}`;
-  const queryTwo = gql`query Two {two}`;
-
   beforeEach(() => {
-    mockClient = createMockClient();
+    jest.spyOn(console, 'warn');
+    jest.spyOn(console, 'error');
   });
 
-  describe('Given a request handler has been defined', () => {
-    let requestHandlerOne: jest.Mock;
-    let resolveRequestOne: Function;
+  afterEach(() => {
+    expect(console.warn).not.toHaveBeenCalled();
+    expect(console.error).not.toHaveBeenCalled();
+  });
 
-    beforeEach(() => {
-      requestHandlerOne = jest.fn(() => new Promise((r) => { resolveRequestOne = r }));
+  describe('Basic tests', () => {
+    describe('Given a request handler has been defined', () => {
+      const queryOne = gql`query One {one}`;
+      const queryTwo = gql`query Two {two}`;
 
-      mockClient.setRequestHandler(queryOne, requestHandlerOne);
-    });
+      let requestHandlerOne: jest.Mock;
+      let resolveRequestOne: Function;
 
-    describe('query', () => {
-      describe('when request handler is defined', () => {
-        let promise: Promise<ApolloQueryResult<any>>;
+      beforeEach(() => {
+        mockClient = createMockClient();
 
-        beforeEach(() => {
-          promise = mockClient.query({ query: queryOne });
-        });
+        requestHandlerOne = jest.fn(() => new Promise((r) => { resolveRequestOne = r }));
 
-        it('returns a promise which resolves to the correct value', async () => {
-          expect(promise).toBeInstanceOf(Promise);
-
-          resolveRequestOne({ data: { one: 'one' } });
-
-          const actual = await promise;
-
-          expect(actual).toEqual(expect.objectContaining({ data: { one: 'one' } }));
-        });
+        mockClient.setRequestHandler(queryOne, requestHandlerOne);
       });
 
-      describe('when request handler is not defined', () => {
-        let promise: Promise<ApolloQueryResult<any>>;
+      describe('query', () => {
+        describe('when request handler is defined', () => {
+          let promise: Promise<ApolloQueryResult<any>>;
 
-        beforeEach(() => {
-          promise = mockClient.query({ query: queryTwo });
+          beforeEach(() => {
+            promise = mockClient.query({ query: queryOne });
+          });
+
+          it('returns a promise which resolves to the correct value', async () => {
+            expect(promise).toBeInstanceOf(Promise);
+
+            resolveRequestOne({ data: { one: 'one' } });
+
+            const actual = await promise;
+
+            expect(actual).toEqual(expect.objectContaining({ data: { one: 'one' } }));
+          });
         });
 
-        it('returns a promise which rejects due to handler not being defined', async () => {
-          expect(promise).toBeInstanceOf(Promise);
+        describe('when request handler is not defined', () => {
+          let promise: Promise<ApolloQueryResult<any>>;
 
-          await expect(promise).rejects.toThrowError('Request handler not defined for query');
+          beforeEach(() => {
+            promise = mockClient.query({ query: queryTwo });
+          });
+
+          it('returns a promise which rejects due to handler not being defined', async () => {
+            expect(promise).toBeInstanceOf(Promise);
+
+            await expect(promise).rejects.toThrowError('Request handler not defined for query');
+          });
         });
+      });
+    });
+  });
+
+  describe('Cache', () => {
+    describe('Given cache has data', () => {
+      const query = gql`query GetComment($id:Number!) { comment(id: $id) { id, text } }`;
+
+      beforeEach(async () => {
+        mockClient = createMockClient();
+
+        mockClient.setRequestHandler(
+          query,
+          () => Promise.resolve({ data: { comment: { id: 1, text: 'aa' } } }),
+        );
+
+        await mockClient.query({ query, variables: { id: 1 } });
+      });
+
+      it('readQuery returns correct data', async () => {
+        const actual = mockClient.readQuery({ query, variables: { id: 1 } });
+
+        expect(actual).toEqual({ comment: { id: 1, text: 'aa' } });
       });
     });
   });
