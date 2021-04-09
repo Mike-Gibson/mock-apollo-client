32fc5bc7e7b3828063345fecff28e0e616a04728
Emile Fugulin <code@efugulin.com>
2021-04-09 14:44:06 -0400

    Add warning for subscriptions bad state

--- src/mockSubscription.ts
+++ src/mockSubscription.ts
@@ -8,10 +8,24 @@ export interface IMockSubscription<TData = any> {
   complete: () => void;
 }
 
+export type MockSubscriptionOptions = {
+  logging?: boolean
+}
+
 export class MockSubscription<TData = any> implements IMockSubscription<TData> {
   private observer?: ZenObservable.SubscriptionObserver<FetchResult>;
+  private logging: boolean;
+
+  constructor(options?: MockSubscriptionOptions) {
+    this.logging = options?.logging ?? true;
+  }
 
   subscribe(observer: ZenObservable.SubscriptionObserver<FetchResult>) {
+    if (this.observer && this.logging) {
+      console.warn(
+        "Warning: mock-apollo-client - Subscription observer should probably not be overriden"
+      );
+    }
     this.observer = observer;
   }
 
@@ -20,17 +34,34 @@ export class MockSubscription<TData = any> implements IMockSubscription<TData> {
   }
 
   next(value: RequestHandlerResponse<TData>) {
+    this.verifyState();
     this.observer?.next(value);
   }
 
   error(errorValue: any) {
+    this.verifyState();
     this.observer?.error(errorValue);
   }
 
   complete() {
+    this.verifyState();
     this.observer?.complete();
+  }
+
+  private verifyState() {
+    if (this.logging) {
+      if (!this.observer) {
+        console.warn(
+          "Warning: mock-apollo-client - Subscription has no observer, this will have no effect"
+        );
+      } else if (this.closed) {
+        console.warn(
+          "Warning: mock-apollo-client - Subscription is closed, this will have no effect"
+        );
+      }
+    }
   }
 }
 
-export const createMockSubscription = <TData = any>() =>
-  new MockSubscription<TData>() as IMockSubscription<TData>;
+export const createMockSubscription = <TData = any>(options?: MockSubscriptionOptions) =>
+  new MockSubscription<TData>(options) as IMockSubscription<TData>;
