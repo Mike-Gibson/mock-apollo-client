a2f5c825366fe52b57a12839dc320d1795607cea
Mike Gibson <mike_gibson@live.co.uk>
2024-04-10 20:47:56 +0100

    chore: update @apollo/client to latest version

--- runTestSuite.sh
+++ runTestSuite.sh
@@ -2,6 +2,7 @@
 
 # This script loops through a list of apollo client npm package versions
 # and runs the jest tests for each version.
+# Currently testing the latest release of each minor version.
 
 apolloVersions=(
   "@apollo/client@3.0.2"
@@ -10,7 +11,10 @@ apolloVersions=(
   "@apollo/client@3.3.21"
   "@apollo/client@3.4.17"
   "@apollo/client@3.5.10"
-  "@apollo/client@3.6.9"
+  "@apollo/client@3.6.10"
+  "@apollo/client@3.7.17"
+  "@apollo/client@3.8.10"
+  "@apollo/client@3.9.11"
 )
 exitStatus=0
 
