dcb9f0aeb4bf1c73507b31f279f0965ffdb63ace
Mike Gibson <mike_gibson@live.co.uk>
2025-11-08 14:43:31 +0000

    style: fix formatting

--- example/src/main.jsx
+++ example/src/main.jsx
@@ -1,9 +1,9 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
-import { ApolloProvider } from '@apollo/client/react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: 'https://localhost:5000/graphql' }),
@@ -16,4 +16,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </ApolloProvider>
   </StrictMode>,
-)
+);
